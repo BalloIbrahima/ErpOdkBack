e6e33acfab8f820a16420877d5bb91b5ec97f9b8
Ibrahima BALLO <ibrahimaballo01@gmail.com>
2022-09-22 15:09:55 +0000

    enlevement de l'autre new

--- src/main/java/com/odc/Apiodkerp/Models/Activite.java
+++ src/main/java/com/odc/Apiodkerp/Models/Activite.java
@@ -19,47 +19,47 @@ import java.util.List;
 @Setter
 public class Activite {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private long id;
-   private String nom;
-   private Date dateCreation;
-   private Date dateDebut;
-   private Date dateFin;
-   private String lieu;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nom;
+    private Date dateCreation;
+    private Date dateDebut;
+    private Date dateFin;
+    private String lieu;
 
-   @Lob
-   private String description;
-   private String image;
+    @Lob
+    private String description;
+    private String image;
 
-   @JsonIgnore
-   @ManyToMany(mappedBy = "activitesFormateurs")
-   List<Utilisateur> utilisateurs = new ArrayList<>();
+    @JsonIgnore
+    @ManyToMany(mappedBy = "activitesFormateurs")
+    List<Utilisateur> utilisateurs = new ArrayList<>();
 
-   @OneToOne(mappedBy = "activite", cascade = CascadeType.ALL)
-   private Tirage tirage;
-   @ManyToOne
-   @JoinColumn(name = "createur")
-   private Utilisateur createur;
+    @OneToOne(mappedBy = "activite", cascade = CascadeType.ALL)
+    private Tirage tirage;
+    @ManyToOne
+    @JoinColumn(name = "createur")
+    private Utilisateur createur;
 
-   @ManyToOne
-   @JoinColumn(name = "lead")
-   private Utilisateur leader;
+    @ManyToOne
+    @JoinColumn(name = "lead")
+    private Utilisateur leader;
 
-   @ManyToOne
-   @JoinColumn(name = "typeactivite")
-   private TypeActivite typeActivite;
+    @ManyToOne
+    @JoinColumn(name = "typeactivite")
+    private TypeActivite typeActivite;
 
-   @ManyToOne
-   @JoinColumn(name = "salle")
-   private Salle salle;
+    @ManyToOne
+    @JoinColumn(name = "salle")
+    private Salle salle;
 
-   @ManyToOne
-   @JoinColumn(name = "etat")
-   private Etat etat;
-   
+    @ManyToOne
+    @JoinColumn(name = "etat")
+    private Etat etat;
+
     @JsonIgnore
     @OneToMany(mappedBy = "activite")
-    List<Presence> presences=new new ArrayList<>();
+    List<Presence> presences = new ArrayList<>();
 
 }
