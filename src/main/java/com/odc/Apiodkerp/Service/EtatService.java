537a687e874a2555a5c3d311639d3ab84e2dba48
RootAba <abassemaiga403@gmail.com>
2022-09-22 14:26:09 +0000

    Ajout des services pour Activite ,entite, et Etat

--- src/main/java/com/odc/Apiodkerp/Service/EtatService.java
+++ src/main/java/com/odc/Apiodkerp/Service/EtatService.java
@@ -1,4 +1,19 @@
 package com.odc.Apiodkerp.Service;
 
+import com.odc.Apiodkerp.Models.Activite;
+import com.odc.Apiodkerp.Models.Etat;
+
+import java.util.List;
+
 public interface EtatService {
+
+    Etat Create(Etat etat);
+
+    List<Etat> GetAll();
+
+    Etat Update(long id,Etat etat);
+
+    String Delete(long id);
+
+    Etat GetById(long id);
 }
