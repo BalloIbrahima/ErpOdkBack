b23d671853b48428d92d1a135a2c0760dad3f55f
mttraore <marytra22@gmail.com>
2022-09-22 10:40:49 +0000

    creation des repository

---
+++ src/main/java/com/odc/Apiodkerp/Repository/EtatRepository.java
@@ -0,0 +1,6 @@
+package com.odc.Apiodkerp.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EtatRepository extends JpaRepository<Long, EtatRepository> {
+}
---
+++ src/main/java/com/odc/Apiodkerp/Repository/SalleRepository.java
@@ -0,0 +1,7 @@
+package com.odc.Apiodkerp.Repository;
+
+import com.odc.Apiodkerp.Models.Salle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalleRepository extends JpaRepository<Long, Salle> {
+}
