d31ab9bc58e14e22033900aaac140796eec7accf
mttraore <marytra22@gmail.com>
2022-09-22 14:25:09 +0000

    service et son implementation

--- src/main/java/com/odc/Apiodkerp/Repository/ListePostulantRepository.java
+++ src/main/java/com/odc/Apiodkerp/Repository/ListePostulantRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ListePostulantRepository extends JpaRepository<ListePostulant, Long> {
+    ListePostulant findByLibelle(String libelle);
 }
