90696b8dfa5ece33032a5220f631244182ceb82e
Ibrahima BALLO <ibrahimaballo01@gmail.com>
2022-09-28 16:13:19 +0000

    service et service impl update

---
+++ src/main/java/com/odc/Apiodkerp/Repository/TacheRepository.java
@@ -0,0 +1,9 @@
+package com.odc.Apiodkerp.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.odc.Apiodkerp.Models.Tache;
+
+public interface TacheRepository extends JpaRepository<Tache, Long> {
+
+}
