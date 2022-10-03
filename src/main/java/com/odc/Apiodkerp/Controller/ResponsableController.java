47d38406c36d73479606eafe8f5abe63d000f2a1
Ibrahima BALLO <ibrahimaballo01@gmail.com>
2022-10-03 21:31:01 +0000

    tache delete

--- src/main/java/com/odc/Apiodkerp/Controller/ResponsableController.java
+++ src/main/java/com/odc/Apiodkerp/Controller/ResponsableController.java
@@ -823,4 +823,47 @@ public class ResponsableController {
 
     }
 
+    @ApiOperation(value = "Afficher tous les taches")
+    @PostMapping("/taches/{idtache}")
+    public ResponseEntity<Object> deleteTache(@RequestParam(value = "user") String userVenant,
+            @PathVariable("idtache") Long idtache) {
+        try {
+
+            Utilisateur utilisateurs = new JsonMapper().readValue(userVenant, Utilisateur.class);
+
+            Utilisateur utilisateur = utilisateurService.trouverParLoginAndPass(utilisateurs.getLogin(),
+                    utilisateurs.getPassword());
+            Droit deleteTache = droitService.GetLibelle("Delete Tache");
+
+            if (utilisateur != null) {
+                if (utilisateur.getRole().getDroits().contains(deleteTache)) {
+                    //
+                    tacheService.delete(idtache);
+                    Historique historique = new Historique();
+                    historique.setDatehistorique(new Date());
+                    historique.setDescription(utilisateur.getPrenom() + " " + utilisateur.getNom()
+                            + " a affiche l'ensemble des taches.");
+
+                    historiqueService.Create(historique);
+
+                    //
+                    return ResponseMessage.generateResponse("ok", HttpStatus.OK, activite.getTache());
+
+                } else {
+                    return ResponseMessage.generateResponse("error", HttpStatus.OK, "Non autorise !");
+
+                }
+            } else {
+                return ResponseMessage.generateResponse("error", HttpStatus.OK, "Cet utilisateur n'existe pas !");
+
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            return ResponseMessage.generateResponse("error", HttpStatus.OK, e.getMessage());
+
+        }
+
+    }
+
 }
