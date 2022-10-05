5477f59d18f2df077115baeac9c0e0c0879cfa6d
Ibrahima BALLO <ibrahimaballo01@gmail.com>
2022-10-05 22:19:20 +0000

    push apres pull mary

--- src/main/java/com/odc/Apiodkerp/Controller/ResponsableController.java
+++ src/main/java/com/odc/Apiodkerp/Controller/ResponsableController.java
@@ -40,6 +40,7 @@ import com.odc.Apiodkerp.Models.Activite;
 import com.odc.Apiodkerp.Models.AouP;
 import com.odc.Apiodkerp.Models.Droit;
 import com.odc.Apiodkerp.Models.Historique;
+import com.odc.Apiodkerp.Models.IntervenantExterne;
 import com.odc.Apiodkerp.Models.ListePostulant;
 import com.odc.Apiodkerp.Models.Postulant;
 
@@ -907,4 +908,63 @@ public class ResponsableController {
 
     }
 
+
+    // ---------------------------CRUD
+    // INTERVENANT EXTERNE-------------------------------------------------------------->
+    @ApiOperation(value = "Creer un intervenant interne.")
+    @PostMapping("/create/intervenant")
+    public ResponseEntity<Object> createIntervenant(@RequestParam(value = "data") String data,
+            @RequestParam(value = "user") String userVenant) {
+        try {
+
+            Utilisateur utilisateu = new JsonMapper().readValue(userVenant, Utilisateur.class);
+
+            IntervenantExterne utilisateur = new JsonMapper().readValue(data, IntervenantExterne.class);
+
+            // Role role = RoleService.GetByLibelle("USER");
+
+            Utilisateur users = utilisateurService.trouverParLoginAndPass(utilisateu.getLogin(),
+                    utilisateu.getPassword());
+            Droit CUser = droitService.GetLibelle("Create Intervenant");
+
+            if (users != null) {
+                if (users.getRole().getDroits().contains(CUser)) {
+
+                    if (intervenantExterneService.getByEmail(utilisateur.getEmail()) == null) {
+                       
+                        try {
+                            Historique historique = new Historique();
+                            Date datehisto = new Date();
+                            historique.setDatehistorique(datehisto);
+                            historique.setDescription("" + users.getPrenom() + " " + users.getNom()
+                                    + " a cree un utilisateur du nom de " + utilisateur.getNom());
+                            historiqueService.Create(historique);
+
+                            IntervenantExterne NewUser = intervenantExterneService.creer(utilisateur);
+                            //System.out.println(NewUser.getLogin());
+                            return ResponseMessage.generateResponse("ok", HttpStatus.OK, NewUser);
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                            return ResponseMessage.generateResponse("iciiii", HttpStatus.OK, e.getMessage());
+
+                        }
+
+                    } else {
+                        return ResponseMessage.generateResponse("error", HttpStatus.OK, "Adresse mail existante");
+
+                    }
+
+                } else {
+                    return ResponseMessage.generateResponse("error", HttpStatus.OK, "Non autorisé");
+
+                }
+            } else {
+                return ResponseMessage.generateResponse("error", HttpStatus.OK, "Cet utilisateur n'existe pas !");
+
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            return ResponseMessage.generateResponse("error", HttpStatus.OK, e.getMessage());
+        }
+    }
 }
